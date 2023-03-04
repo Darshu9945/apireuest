4f32f19400290507b3fb12b5717171d9c2565ec1
Darshu9945 <120715519+Darshu9945@users.noreply.github.com>
2023-03-04 19:39:08 +0530

    Add files via upload

---
+++ 1st apirequestapi.js
@@ -0,0 +1,87 @@
+const btn = document.getElementById('my-form')
+var nameinput=document.getElementById('kb3')
+var emailinput=document.getElementById('kb2')
+var msg =document.getElementById('kb5')
+var userlist=document.getElementById('mylist')
+
+btn.addEventListener('submit',adduser);
+
+
+
+ function adduser(e){
+
+    e.preventDefault();
+
+    if (nameinput.value==='' || emailinput.value===''){
+
+        msg.calasslist.add('error')
+
+        msg.innerHTML='please enter all the field';
+
+    }
+
+    else{
+        name=nameinput.value
+        email=emailinput.value
+        const obj={
+            name,
+            email
+        }
+        axios.post("https://crudcrud.com/api/34ba1a7f54314ee4b16f68e239f5f044/appoitmentdata",obj).then((responce)=>{
+            const li =document.createElement('li')
+            
+            console.log(responce.data)
+        }
+        ).catch((err)=>{
+            console.log(err)
+        })
+
+        localStorage.setItem(emailinput.value,JSON.stringify(obj))
+
+        // localStorage.setItem('email',emailinput.value)
+
+        // let nameget=JSON.parse(localStorage.getItem('name'))
+
+        // console.log(nameget)
+
+        Object.keys(localStorage).forEach((key) => {
+
+                k1=localStorage.getItem(key);
+
+                detailsofpepole=JSON.parse(k1)
+
+                // console.log(Object.keys(detailsofpepole))
+
+                let b1 = Object.keys(detailsofpepole)
+
+                const li =document.createElement('li')
+
+                  b1.forEach(key => {
+
+                    
+
+                    var k3 =document.createTextNode(`${detailsofpepole[key]}  : `)
+
+                    li.appendChild(k3)
+
+                
+
+                  console.log(detailsofpepole[key])
+
+            }
+
+                )
+
+                // console.log(detailsofpepole,'kb')
+
+                userlist.appendChild(li)
+
+        }
+
+        )
+
+
+
+    }
+
+ }
